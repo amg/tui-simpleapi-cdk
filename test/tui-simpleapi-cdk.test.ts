634eaa633eab159e0076d459294fb4015723846b
Alex G <gvozdik.sanya@gmail.com>
2025-03-22 11:24:09 +1100

    Initial commit

---
+++ test/tui-simpleapi-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TuiSimpleapiCdk from '../lib/tui-simpleapi-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tui-simpleapi-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TuiSimpleapiCdk.TuiSimpleapiCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
